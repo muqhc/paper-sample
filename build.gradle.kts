3c25b4f44dfc905238f431b702dd3866461ec40c
muqhc <88622655+muqhc@users.noreply.github.com>
2023-03-04 07:04:56 +0000

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,20 @@ dependencies {
     implementation("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
-
+tasks.processResources {
+    val apiVersion = rootProject.providers.gradleProperty("mcVersion").get()
+        .split(".", "-").take(2).joinToString(".")
+    val props = mapOf(
+        "version" to project.version,
+        "apiVersion" to apiVersion,
+        "pluginName" to project.name.split('-').joinToString("") { it.capitalize() },
+        "group" to (project.group as String)
+        
+    )
+    inputs.properties(props)
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
 
 
